80055aa545f314a9b860b29ed417a2857d823be4
Nateheff <nateheffgk05@gmail.com>
2025-05-15 14:58:51 -0700

    better vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/<REPO>/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        posts: resolve(__dirname, "posts.html"),
+        projects: resolve(__dirname, "projects.html"),
+      },
+    },
+  },
+});
